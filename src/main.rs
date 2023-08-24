f212b41442d77957c4d46fcd21ac15d6c01b446e
Adeoye Adefemi <adefemiadeoye@yahoo.com>
2023-08-24 03:06:22 +0100

    refactor

--- src/main.rs
+++ src/main.rs
@@ -1,46 +1,11 @@
-use clap::{Parser, Subcommand};
-use commands::{
-    download::DownloadCommands, email::EmailCommands, gitignore::GitIgnoreCommands,
-    readme::ReadmeCommands, sms::SmsCommands,
-};
 use include_dir::{include_dir, Dir};
-
+use parser::Utils;
 pub const SOURCE_DIR: Dir = include_dir!("src/templates");
 
 mod commands;
+mod parser;
 mod utils;
-#[derive(Parser)]
-#[command(author, version, about, long_about = None)]
-#[command(propagate_version = true)]
-struct Utils {
-    #[command(subcommand)]
-    command: Commands,
-}
-
-#[derive(Subcommand)]
-enum Commands {
-    /// download files, videos, etc
-    Download(DownloadCommands),
-    /// send email
-    Email(EmailCommands),
-    /// generate project readmes
-    Readme(ReadmeCommands),
-    ///send SMS
-    Sms(SmsCommands),
-    /// handle git operations
-    // Git(GitCommands),
-    /// include .gitignore
-    GitIgnore(GitIgnoreCommands),
-}
 
 fn main() {
-    let utils = Utils::parse();
-
-    match utils.command {
-        // Commands::Download(download) => DownloadCommands::parse(),
-        Commands::GitIgnore(git_ignore) => git_ignore.parse(),
-        // Commands::Git(git) => git.parse(),
-        Commands::Readme(readme) => readme.parse(),
-        _ => panic!(),
-    }
+    Utils::run();
 }
--- src/parser.rs
+++ src/parser.rs
@@ -1,16 +1,43 @@
-/// the core parser
-/// the Utils struct shall bear other subcommands 
+use clap::{Parser, Subcommand};
+use commands::{
+    download::DownloadCommands, email::EmailCommands, gitignore::GitIgnoreCommands,
+    readme::ReadmeCommands, sms::SmsCommands,
+};
+
+use crate::commands;
+
+#[derive(Parser)]
+#[command(author, version, about, long_about = None)]
+#[command(propagate_version = true)]
 pub struct Utils {
-    //sub commands
-    command: UtilSubCommands,
-    // add config parser 
+    #[command(subcommand)]
+    pub command: Commands,
 }
 
-/// the utils sub commands
-pub enum UtilSubCommands {
-    /// the readme parser
-    
-    /// the gitignore parser
-    /// the youtube video downloader 
-    /// git and github repository parser
+impl Utils {
+    pub fn run() {
+        let utils = Utils::parse();
+
+        match utils.command {
+            Commands::GitIgnore(git_ignore) => git_ignore.parse(),
+            Commands::Readme(readme) => readme.parse(),
+            _ => panic!(),
+        }
+    }
+}
+
+#[derive(Subcommand)]
+pub enum Commands {
+    /// download files, videos, etc
+    Download(DownloadCommands),
+    /// send email
+    Email(EmailCommands),
+    /// generate project readmes
+    Readme(ReadmeCommands),
+    ///send SMS
+    Sms(SmsCommands),
+    /// handle git operations
+    // Git(GitCommands),
+    /// include .gitignore
+    GitIgnore(GitIgnoreCommands),
 }
