576575d8f8b8d138da1129845db17120b454631c
Adeoye Adefemi <adefemiadeoye@yahoo.com>
2023-08-26 11:13:11 +0100

    feat:config database

---
+++ src/database.rs
@@ -0,0 +1,30 @@
+use sqlite::Connection;
+
+// pub struct Database<'a> {
+//     path: &'a str,
+// }
+
+pub struct Database;
+
+impl Database {
+    pub fn init() -> Connection {
+        let connection = sqlite::open("./utils.db").unwrap();
+
+        // create the email table
+        let email_create_table =
+            "CREATE TABLE IF NOT EXISTS emails ( id VARCHAR PRIMARY KEY, name VARCHAR, email VARCHAR, message TEXT date TEXT)";
+
+        let store_create_table =
+            "CREATE TABLE IF NOT EXIST store (id VARCHAR, key VARCHAR, value TEXT, date TEXT)";
+
+        connection.execute(email_create_table).unwrap();
+        connection.execute(store_create_table).unwrap();
+
+        connection
+    }
+
+    /// return connection to the database;
+    pub fn conn() -> Connection {
+        Self::init()
+    }
+}
