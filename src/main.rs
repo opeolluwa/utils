b37eb35fb3396865fb8b4910f163861358e36c61
Adeoye Adefemi <adefemiadeoye@yahoo.com>
2023-08-28 07:02:00 +0100

    fixes: fixed having multiple databases
    the previous implementation creates multiple database this has been resolved by using a single shared db in the OS home dir

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,24 @@
+use lazy_static::lazy_static;
+
 use include_dir::{include_dir, Dir};
 
 pub const SOURCE_DIR: Dir = include_dir!("src/templates");
+lazy_static! {
+    pub static ref DB_URL: std::string::String = {
+          // create the database in the home directory of the system
+        //create "utils" directory in the home dir and / save files to $HOME/utils;
+        let os_default_home_dir = dirs::home_dir().unwrap();
+        let db_path = format!(
+            "{home_dir}/{upload_dir}",
+            home_dir = os_default_home_dir.display(),
+            upload_dir = "utils"
+        );
+        // create the path if not exist path if not exist
+        let _ = std::fs::create_dir_all(&db_path);
 
+    format!("sqlite://{db_path}/utils.db")
+    };
+}
 mod commands;
 mod database;
 mod parser;
