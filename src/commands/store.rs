90f56bc542466178da205eb0be8da10c6e388326
adeoye adefemi <adefemiadeoye@yahoo.com>
2024-03-22 15:33:56 +0100

    feat(auth): add security questionauthorization

--- src/commands/store.rs
+++ src/commands/store.rs
@@ -1,12 +1,23 @@
+use crate::security_questions::{self, security_questions};
+use dialoguer::{theme::ColorfulTheme, Input, Select};
+use entity::password;
+use password::Entity as Password;
 use std::time::Duration;
 
-use crate::{style::PrintColoredText, DB_URL};
+use crate::{style::LogMessage, DB_URL};
 use anyhow::{Ok, Result};
 use bcrypt::{hash, verify, DEFAULT_COST};
 use chrono::Local;
 use clap::{Args, Subcommand};
+use dialoguer::{Confirm, Password as PassPhrase};
 use entity::store::{self, Entity as Store};
-use sea_orm::{ActiveValue::Set, ConnectOptions, Database, DatabaseConnection, EntityTrait};
+use sea_orm::{
+    ActiveModelTrait, ColumnTrait, ConnectionTrait, DatabaseBackend, ExecResult, Statement,
+};
+use sea_orm::{
+    ActiveValue::Set, Condition, ConnectOptions, Database, DatabaseConnection, EntityTrait,
+    ModelTrait, QueryFilter, QueryOrder,
+};
 use serde::{Deserialize, Serialize};
 #[derive(Args, Debug, Serialize, Deserialize)]
 pub struct StoreCommands {
@@ -23,13 +34,18 @@ pub enum SubCommands {
     /// list the stored data
     List,
     /// delete a key
-    Delete { key: String },
+    Delete {
+        key: String,
+    },
     /// clear all stored data
     Clear,
     /// update the value of a key
-    Update { key: String, value: String },
-    // secure the stored data
-    // Secure,
+    Update {
+        key: String,
+        value: String,
+    },
+    // secure the stored data with a password
+    Secure,
 }
 
 impl StoreCommands {
@@ -40,15 +56,15 @@ impl StoreCommands {
                 SubCommands::Delete { key } => Self::delete(key).await,
                 SubCommands::Clear => Self::clear().await,
                 SubCommands::Update { key, value } => Self::update(key, value).await,
-                // SubCommands::Secure => Self::secure().await,
+                SubCommands::Secure => Self::secure().await,
             }
         } else {
             let Some(key) = &self.key else {
-                PrintColoredText::error("Invalid key");
+                LogMessage::error("Invalid key");
                 std::process::exit(0);
             };
             let Some(value) = &self.value else {
-                PrintColoredText::error("Invalid value");
+                LogMessage::error("Invalid value");
                 std::process::exit(0);
             };
             let date_added = Local::now().to_rfc2822();
@@ -65,9 +81,8 @@ impl StoreCommands {
                 .exec(&Self::db_connection().await?)
                 .await?;
 
-            // Store::new(key, value).save().await.unwrap();
             let message = format!("{key} successfully stored");
-            PrintColoredText::success(&message);
+            LogMessage::success(&message);
             Ok(())
         }
     }
@@ -77,7 +92,7 @@ impl StoreCommands {
             Store::find().all(&Self::db_connection().await?).await?;
 
         if data.is_empty() {
-            PrintColoredText::error("no data found");
+            LogMessage::error("no data found");
             std::process::exit(0)
         }
 
@@ -89,20 +104,102 @@ impl StoreCommands {
 
     /// remove record from the store  
     async fn delete(key: &str) -> Result<()> {
+        let record = Self::find_one(key).await?;
+        let _ = record.delete(&Self::db_connection().await?).await;
+        let message = format!("{key} successfully deleted");
+        LogMessage::success(&message);
         Ok(())
     }
 
     /// update recoird n the store
     async fn update(key: &str, value: &str) -> Result<()> {
-        // let _ = crate::database::Store::update(key, value).await.ok();
+        let mut record: entity::store::ActiveModel = Self::find_one(key).await?.into();
+        record.value = Set(value.to_owned());
+
+        let _: entity::store::Model = record.update(&Self::db_connection().await?).await?.into();
 
         let message = format!("{key} successfully updated");
-        PrintColoredText::success(&message);
+        LogMessage::success(&message);
+
         Ok(())
     }
 
+    /// delte all record in the database
     async fn clear() -> Result<()> {
-        // crate::database::Store::clear().await;
+        // prompt for confirmation and Password if the user choose to continue
+        if Confirm::with_theme(&ColorfulTheme::default())
+            .with_prompt(
+                "The action will remove all the stored data\nDo you really want to continue?",
+            )
+            .default(true)
+            .interact()
+            .unwrap()
+        {
+            let password = PassPhrase::with_theme(&ColorfulTheme::default())
+                .with_prompt("Password")
+                .interact()
+                .unwrap();
+
+            // validate the password
+            if password != "opeolluwa" {
+                LogMessage::error("Incorrect password! Exciting...");
+                std::process::exit(1);
+            }
+            // clear the database
+            let _: ExecResult = Self::db_connection()
+                .await?
+                .execute(Statement::from_string(
+                    DatabaseBackend::Sqlite,
+                    r#"DELETE FROM store"#,
+                ))
+                .await?;
+        } else {
+            println!("nevermind then :(");
+            std::process::exit(1)
+        }
+
+        LogMessage::success("Stored successfully flushed");
+
+        Ok(())
+    }
+
+    /// Secure the databse with a password
+    async fn secure() -> Result<()> {
+        // for every run, promtp the user to set up security question if not exist
+        let authentication_credentials: Option<password::Model> = Password::find_by_id(1)
+            .one(&Self::db_connection().await?)
+            .await?;
+        if authentication_credentials.is_none() {
+            LogMessage::neutral("Provide a security question to proceed");
+
+            let question_index = Select::with_theme(&ColorfulTheme::default())
+                .with_prompt("Provide a passphrase to proceed")
+                .default(0)
+                .items(&security_questions::security_questions()[..])
+                .interact()
+                .ok();
+
+            if let Some(security_question_index) = question_index {
+                let selected_question = security_questions()[security_question_index];
+                let answer: String = Input::with_theme(&ColorfulTheme::default())
+                    .with_prompt("Answer the security question")
+                    .interact_text()
+                    .unwrap();
+
+                let hashed_answer = hash(answer.trim(), DEFAULT_COST)?;
+
+                let record = entity::password::ActiveModel {
+                    id: Set(1),
+                    sequrity_question: Set(selected_question.to_owned()),
+                    answer_hash: Set(hashed_answer),
+                };
+
+                let _ = record.insert(&Self::db_connection().await?).await?;
+                LogMessage::success("Store secured successfully");
+            } else {
+                std::process::exit(1)
+            }
+        }
         Ok(())
     }
 
@@ -121,5 +218,28 @@ impl StoreCommands {
         // the database instance
         let db = Database::connect(opt).await?;
         Ok(db)
+    }
+
+    /// find a record by key
+    async fn find_one(key: &str) -> Result<entity::store::Model> {
+        let record = entity::store::Entity::find()
+            .filter(
+                Condition::all()
+                    .add(entity::store::Column::Key.like(format!("%{}%", key.trim().to_string()))),
+            )
+            .order_by_asc(entity::store::Column::DateAdded)
+            .all(&Self::db_connection().await?)
+            .await?;
+
+        // exit if no record
+        if record.len() == 0 {
+            let message = format!("{key} not found");
+            LogMessage::error(&message);
+            std::process::exit(1);
+        }
+
+        let record = &record[0];
+
+        Ok(record.to_owned())
     }
 }
