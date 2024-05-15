8eb5254c7ec007c47b39b8997f958b506cba6bc8
Adeoye Adefemi <adefemiadeoye@yahoo.com>
2024-05-15 05:19:53 +0100

    chore: enforce responsiveness

--- utils-web/src/pages/home.rs
+++ utils-web/src/pages/home.rs
@@ -7,12 +7,12 @@ use leptos::{component, view, IntoView};
 pub fn Home() -> impl IntoView {
     view! {
         <Layout class="">
-            <header class="h-[75vh] flex flex-col item-center justify-center">
-                <h1 class="text-5xl text-center capitalize font-medium ">
+            <header class="h-[60vh] sm:h-[75vh] flex flex-col item-center justify-center">
+                <h1 class="text-3xl sm:text-5xl text-center capitalize font-medium ">
                     One Stop toolbox for every developer
                 </h1>
-                <p class="leading-2 w-2/3 mx-auto mt-2 py-2 text-center">
-                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Explicabo consectetur facilis, dicta, in magni incidunt esse neque, iste rem libero omnis asperiores earum? Eligendi magnam, ratione harum saepe fugiat similique?
+                <p class="leading-2 sm:w-2/3 mx-auto mt-2 py-2 text-center">
+                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Explicabo consectetur facilis, dicta, in magni incidunt esse neque, 
                 </p>
                 <div class="flex items-center w-fit gap-2 mx-auto my-4 ">
                     <Button class="bg-[#101010] text-white w-fit flex items-center justify-center text-center">
@@ -25,9 +25,9 @@ pub fn Home() -> impl IntoView {
 
                 </div>
             </header>
-            <section class="flex justify-between items-center my-[100px]">
+            <section class="sm:flex justify-between items-center sm:my-[100px]">
 
-                <div class="w-1/2">
+                <div class="sm:w-1/2">
                     <h2 class="font-medium text-3xl leading-3 mb-2">Some heading</h2>
                     <p class="leading-2 my-4">
                         Lorem ipsum, dolor sit amet consectetur adipisicing elit. Temporibus labore beatae eum sed ad natus perspiciatis. Facere molestias incidunt molestiae maxime perferendis consequatur ullam tenetur! Mollitia porro numquam voluptatibus aut?
