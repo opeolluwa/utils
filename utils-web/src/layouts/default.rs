d4c3ed5bec3dcadee08d92ae7b6a33e7b86cd45b
Adeoye Adefemi <adefemiadeoye@yahoo.com>
2024-05-15 11:49:59 +0100

    feat: add dashboard layout

--- utils-web/src/layouts/default.rs
+++ utils-web/src/layouts/default.rs
@@ -4,7 +4,13 @@ use leptos::{component, view, Children, IntoView};
 
 /// the default application layout, with footer, viewbox, and navigation
 #[component]
-pub fn Layout(children: Children, class: &'static str) -> impl IntoView {
+pub fn Layout(
+    /// elements injected into the DOM node
+    children: Children,
+    /// tailwind/css classes
+    #[prop(default = "")]
+    class: &'static str,
+) -> impl IntoView {
     view! {
         <AppNavigation/>
         <main class="container my-8">{children()}</main>
