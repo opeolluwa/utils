29e4ab040ab61bd0165c652fe29461c79138eff0
Adeoye Adefemi <adefemiadeoye@yahoo.com>
2024-06-13 11:53:52 +0100

    feat: add confy

--- utils-cli/src/commands/mod.rs
+++ utils-cli/src/commands/mod.rs
@@ -1,7 +1,7 @@
+pub mod auth;
 pub mod cli;
 pub mod download;
 pub mod gitignore;
 pub mod readme;
 pub mod sms;
 pub mod store;
-pub mod auth;
--- utils-cli/src/config.rs
+++ utils-cli/src/config.rs
@@ -1,67 +1,43 @@
-use anyhow::Result;
-use configparser::ini::Ini;
-use std::io::prelude::*;
-use std::{fs::OpenOptions, io::BufReader};
+use serde::{Deserialize, Serialize};
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Config {
+    pub client_id: String,
+    pub client_secret: String,
+    pub api_version: String,
+    pub store: Store,
+}
 
-use crate::{CONFIG_FILE, SOURCE_DIR};
-pub struct Config;
+#[derive(Serialize, Deserialize, Debug, Default)]
+struct Store {
+    pub list: RequireAuth,
+    pub delete: RequireAuth,
+    pub clear: RequireAuth,
+}
 
-#[allow(dead_code)]
+#[derive(Serialize, Deserialize, Debug)]
+struct RequireAuth {
+    require_auth: bool,
+}
+
+impl Default for RequireAuth {
+    fn default() -> Self {
+        Self { require_auth: true }
+    }
+}
+
+impl Default for Config {
+    fn default() -> Self {
+        Self {
+            client_id: "".to_string(),
+            client_secret: "".to_string(),
+            api_version: "v1".to_string(),
+            store: Default::default(),
+        }
+    }
+}
+
 impl Config {
-    /// return the config path
-    pub fn path() -> Result<String> {
-        // Ok(CONFIG_FILE.as_str().to_string())
-        Ok(CONFIG_FILE.as_str().to_string())
-    }
-
-    /// create a new config if not exist
-    pub fn init() -> Result<()> {
-        //  https://stackoverflow.com/questions/35636742/is-there-any-way-to-create-and-open-a-file-if-it-doesnt-exist-but-fail-otherwis
-        // write the config file
-        let mut file = OpenOptions::new()
-            .create(true)
-            .append(true)
-            .open(CONFIG_FILE.as_str())
-            .unwrap();
-
-        // read the default config file and write to the config file
-        let default_config = SOURCE_DIR.get_file("utils.conf").unwrap();
-        let default_config = default_config.contents_utf8().unwrap();
-        file.write_all(default_config.as_bytes()).unwrap();
-
-        Ok(())
-    }
-
-    /// read a fied from the config
-    /// 
-    /// read the config
-    /// #example
-    /// ```rust
-    ///  let rr: Option<String>=  config::Config::parse_key("port", "server")?;
-    ///  print!("{:?}", rr);
-    /// ````
-    pub fn parse_key(key: &str, section: &str) -> Result<Option<String>> {
-        let raw_config_file = Self::load()?;
-        let mut config = Ini::new();
-
-        // You can easily load a file to get a clone of the map:
-        let _ = config.read(raw_config_file);
-
-        Ok(config.get(section, key))
-    }
-/// read the confog file to string 
-    fn load() -> Result<String> {
-        // read the config file
-        let config = OpenOptions::new()
-            .read(true)
-            .open(CONFIG_FILE.as_str())
-            .unwrap();
-
-        // read the config file
-        let mut reader = BufReader::new(config);
-        let mut buffer = String::new();
-        reader.read_to_string(&mut buffer).unwrap();
-
-        Ok(buffer)
+    pub fn load() -> Config {
+        confy::load("utils", None).unwrap_or_default()
     }
 }
